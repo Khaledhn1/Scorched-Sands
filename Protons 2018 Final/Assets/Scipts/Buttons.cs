7b65bfe3886f156ee1d80bdda7ba3f993b35cc63
Khaled H. Nassef <khalednassef03@gmail.com>
2018-07-25 17:05:31 +0200

    first push

---
+++ Protons 2018 Final/Assets/Scipts/Buttons.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour {
+	public GameObject MM;
+	public GameObject Options_Menu;
+
+	public void Play(){
+		Application.LoadLevel(1);
+	}
+	public void Exit(){
+		if (Application.isEditor){
+			print("cant quit editor m8!");
+		}else{
+			Application.Quit();
+		}
+	}
+	public void Options(){
+		 MM.SetActive(false); 
+		 Options_Menu.SetActive(true);
+	}
+	public void MainMenu(){
+		 MM.SetActive(true); 
+		 Options_Menu.SetActive(false);
+	}
+}
