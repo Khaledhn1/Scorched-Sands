6e6ae10e36360017a3d1dac79c33f549689cc3e3
Khaled H. Nassef <khalednassef03@gmail.com>
2018-07-26 03:00:49 +0200

    Main Menu and some map

---
+++ Protons 2018 Final/Assets/Scipts/GameSettings.cs
@@ -0,0 +1,10 @@
+﻿public class GameSettings{
+	public bool fullscreen;
+	public int quality;
+	public int antialiasing;
+	public int vsync;
+	public int resolution;
+	public float volume;
+	
+	
+}
---
+++ Protons 2018 Final/Assets/Scipts/SettingManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System.IO;
+
+public class SettingManager : MonoBehaviour {
+
+	public Toggle fullscreenToggle;
+	public Dropdown resolutionDropdown;
+	public Dropdown qualityDropdown;
+	public Dropdown AADropdown;
+	public Dropdown VSyncDropdown;
+	public Slider volumeSlider;
+	public Button AcceptButton;
+	
+	public AudioSource audio;
+	public Resolution[] resolutions;
+	public GameSettings gs;
+	public Buttons buttons;
+	
+	public void OnEnable(){
+		gs = new GameSettings();
+		
+		fullscreenToggle.onValueChanged.AddListener(delegate{ OnFullscreenToggle(); });
+		resolutionDropdown.onValueChanged.AddListener(delegate{OnResolutionChange();});
+		qualityDropdown.onValueChanged.AddListener(delegate{OnQualityChange();});
+		AADropdown.onValueChanged.AddListener(delegate{OnAAChange();});
+		VSyncDropdown.onValueChanged.AddListener(delegate{OnVSyncChange();});
+		volumeSlider.onValueChanged.AddListener(delegate{OnVolumeChange();});
+		AcceptButton.onClick.AddListener(delegate{OnApplyClick();});
+		
+		resolutions = Screen.resolutions;
+		foreach(Resolution resolution in resolutions){
+			resolutionDropdown.options.Add(new Dropdown.OptionData(resolution.ToString()));
+		}
+		LoadSettings();
+	}
+	public void OnFullscreenToggle(){
+		gs.fullscreen = Screen.fullScreen = fullscreenToggle.isOn;
+	}
+	public void OnResolutionChange(){
+		Screen.SetResolution(resolutions[resolutionDropdown.value].width,resolutions[resolutionDropdown.value].height,Screen.fullScreen);
+		gs.resolution = resolutionDropdown.value;
+	}
+	public void OnQualityChange(){
+		QualitySettings.masterTextureLimit = gs.quality = qualityDropdown.value;
+	}
+	public void OnAAChange(){
+		QualitySettings.antiAliasing = gs.antialiasing= (int)Mathf.Pow(2f, AADropdown.value);
+	}
+	public void OnVSyncChange(){
+		QualitySettings.vSyncCount = gs.vsync = VSyncDropdown.value;
+	}
+	public void OnVolumeChange(){
+		audio.volume = gs.volume = volumeSlider.value;
+	}
+	public void OnApplyClick(){
+		SaveSettings();
+		buttons.MainMenu();
+	}
+	public  void SaveSettings(){
+		string jsonData = JsonUtility.ToJson(gs, true);
+		File.WriteAllText(Application.persistentDataPath + "/settings.json",jsonData);
+	}
+	public  void LoadSettings(){
+		gs = JsonUtility.FromJson<GameSettings>(File.ReadAllText(Application.persistentDataPath + "/settings.json"));
+		volumeSlider.value = gs.volume;
+		AADropdown.value = gs.antialiasing;
+		qualityDropdown.value = gs.quality;
+		VSyncDropdown.value = gs.vsync;
+		resolutionDropdown.value = gs.resolution;
+		fullscreenToggle.isOn = gs.fullscreen;
+		
+		resolutionDropdown.RefreshShownValue();
+		
+
+	}
+	
+}
