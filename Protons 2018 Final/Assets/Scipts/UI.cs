bed9c8dbb717cdd7623ed5f6cd45ae0230c9f687
Khaled H. Nassef <khalednassef03@gmail.com>
2018-08-02 22:22:15 +0200

    UI!!!

---
+++ Protons 2018 Final/Assets/Scipts/UI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+
+	public Gun1 gun;
+	public Target1 player;
+	public Text CurrentAmmo;
+	public Text MaxAmmo;
+	public Slider health;
+	// Use this for initialization
+	void Start () {
+		health.value = player.health;
+		CurrentAmmo.text = gun.currentAmmo.ToString();
+		MaxAmmo.text = gun.maxAmmo.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		health.value = player.health;
+		CurrentAmmo.text = gun.currentAmmo.ToString();
+		MaxAmmo.text = gun.maxAmmo.ToString();
+	}
+}
