f24448cad9d19368f5896b81d53969b6b2afb20e
Khaled Nassef <khalednassef03@gmail.com>
2018-09-01 02:14:38 +0200

    Added enemy spawn points + map changes + bug fixes

--- Protons 2018 Final/Assets/Scipts/EnemyManager.cs
+++ Protons 2018 Final/Assets/Scipts/EnemyManager.cs
@@ -10,12 +10,13 @@ public class EnemyManager : MonoBehaviour {
 	public float SpawnTime = 3f;
 	GameObject zom;
 	GameObject hum;
-	public Transform[] spawnPoints;
+	private GameObject[] spawnPoints;
 	public DayNightController time;
 	// Use this for initialization
 	void Start () {
 		
 		InvokeRepeating("Spawn", SpawnTime, SpawnTime);
+		spawnPoints = GameObject.FindGameObjectsWithTag("EnemySpawnPoint");
 	}
 	
 	// Update is called once per frame
@@ -24,10 +25,10 @@ public class EnemyManager : MonoBehaviour {
 		int spawnPointIndex = Random.Range(0,spawnPoints.Length);
 		
 	if (time.currentTime < 8.0f || time.currentTime > 21.0f){
-			zom = Instantiate (Zombie,spawnPoints[spawnPointIndex].position,spawnPoints[spawnPointIndex].rotation); 
+			zom = Instantiate (Zombie,spawnPoints[spawnPointIndex].transform.position,spawnPoints[spawnPointIndex].transform.rotation); 
 			zom.SetActive(true);
 		}else{
-			hum = Instantiate (Human,spawnPoints[spawnPointIndex].position,spawnPoints[spawnPointIndex].rotation); 
+			hum = Instantiate (Human,spawnPoints[spawnPointIndex].transform.position,spawnPoints[spawnPointIndex].transform.rotation); 
 			hum.SetActive(true);
 		}
 	}
