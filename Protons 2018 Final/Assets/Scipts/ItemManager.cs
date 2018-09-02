54682212873ff434ac59be99bb5443593893571d
Khaled Nassef <khalednassef03@gmail.com>
2018-09-02 17:48:25 +0200

    Added Item pickups(Health/Ammo)+Bug fixes

---
+++ Protons 2018 Final/Assets/Scipts/ItemManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour {
+
+	public GameObject ammo;
+	public GameObject health;
+	public float SpawnTime = 3f;
+	GameObject he;
+	GameObject am;
+	private GameObject[] spawnPoints;
+	// Use this for initialization
+	void Start () {
+		
+		InvokeRepeating("Spawn", SpawnTime, SpawnTime);
+		spawnPoints = GameObject.FindGameObjectsWithTag("ItemSpawnPoint");
+	}
+	
+	// Update is called once per frame
+	void Spawn() {
+		int spawnPointIndex = Random.Range(0,spawnPoints.Length);
+		int type = Random.Range(0,2);
+		
+	if (type == 0){
+			he = Instantiate (health,spawnPoints[spawnPointIndex].transform.position,spawnPoints[spawnPointIndex].transform.rotation); 
+			he.SetActive(true);
+		}else{
+			am = Instantiate (ammo,spawnPoints[spawnPointIndex].transform.position,spawnPoints[spawnPointIndex].transform.rotation); 
+			am.SetActive(true);
+		}
+	}
+} 
---
+++ Protons 2018 Final/Assets/Scipts/Pickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+	[Range(1,50)]
+	public int amount;
+	
+	public Target PlayerHealth;
+	public Gun m4;
+	public Gun m16;
+	public GameObject m4go;
+	public GameObject m16go;
+	// Use this for initialization
+
+	void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag ("Ammo"))
+        {
+            Destroy(other.gameObject);
+			if(m4go.active)m4.maxAmmo += amount;
+			if(m16go.active)m16.maxAmmo += amount;
+        }
+		if (other.gameObject.CompareTag ("Health"))
+        {
+            Destroy(other.gameObject);
+			PlayerHealth.health += amount;
+        }
+    }
+
+}
