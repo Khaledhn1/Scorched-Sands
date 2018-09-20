29dc1a2012da2c13a3321fcd38804456ca1e0bba
Khaled Nassef <khalednassef03@gmail.com>
2018-09-20 17:34:43 +0200

    Fixed streets + bug fixes

--- Protons 2018 Final/Assets/Standard Assets/Vehicles/Car/Scripts/CarAudio.cs
+++ Protons 2018 Final/Assets/Standard Assets/Vehicles/Car/Scripts/CarAudio.cs
@@ -87,18 +87,18 @@ namespace UnityStandardAssets.Vehicles.Car
         private void Update()
         {
             // get the distance to main camera
-            //float camDist = (Camera.main.transform.position - transform.position).sqrMagnitude;
+            float camDist = (Camera.main.transform.position - transform.position).sqrMagnitude;
 
             // stop sound if the object is beyond the maximum roll off distance
-            //if (m_StartedSound && camDist > maxRolloffDistance*maxRolloffDistance)
+            if (m_StartedSound && camDist > maxRolloffDistance*maxRolloffDistance)
             {
-                //StopSound();
+                StopSound();
             }
 
              //start the sound if not playing and it is nearer than the maximum distance
-            //if (!m_StartedSound && camDist < maxRolloffDistance*maxRolloffDistance)
+            if (!m_StartedSound && camDist < maxRolloffDistance*maxRolloffDistance)
             {
-                //StartSound();
+                StartSound();
             }
 
             if (m_StartedSound)
